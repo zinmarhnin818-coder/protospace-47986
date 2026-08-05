2eb58aaf75390d2f6a0d3962563df2c42a82e3cb
zinmarhnin818-coder <zinmarhnin818@gmail.com>
2026-08-05 21:22:22 +0900

    投稿機能の実装

--- Gemfile
+++ Gemfile
@@ -67,3 +67,5 @@ group :test do
   gem "selenium-webdriver"
 end
 gem 'devise'
+gem 'mini_magick'
+gem 'image_processing', '~> 1.2'
--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,4 +1,24 @@
 class PrototypesController < ApplicationController
   def index
+    @prototypes = Prototype.all
+  end
+
+  def new
+    @prototype = Prototype.new
+  end
+
+  def create
+    @prototype = Prototype.new(prototype_params)
+    if @prototype.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def prototype_params
+    params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
   end
 end
