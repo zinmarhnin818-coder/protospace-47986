19d4cf40ebd2ee7099d3b311dc33f3513db477e2
zinmarhnin818-coder <zinmarhnin818@gmail.com>
2026-08-08 15:49:10 +0900

    ユーザー詳細ページの実装

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,8 @@ Rails.application.routes.draw do
   resources :prototypes, only: [:index, :new, :create, :show, :edit, :update, :destroy] do
     resources :comments, only: :create
   end
-  
+  resources :users, only: :show
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
