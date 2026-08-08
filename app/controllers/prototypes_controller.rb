e30eea81470e2e61631ae94111de01f79f9769c4
zinmarhnin818-coder <zinmarhnin818@gmail.com>
2026-08-08 16:20:56 +0900

    ユーザーのアクセス範囲の制限

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,12 +1,15 @@
 class PrototypesController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :move_to_index, only: [:edit]
+
   def index
     @prototypes = Prototype.all
   end
 
   def show
     @prototype = Prototype.find(params[:id])
-  @comment = Comment.new
-  @comments = @prototype.comments.includes(:user)
+    @comment = Comment.new
+    @comments = @prototype.comments.includes(:user)
   end
 
   def new
@@ -34,15 +37,23 @@ class PrototypesController < ApplicationController
       render :edit, status: :unprocessable_entity
     end
   end
+
   def destroy
-  prototype = Prototype.find(params[:id])
-  prototype.destroy
-  redirect_to root_path
+    prototype = Prototype.find(params[:id])
+    prototype.destroy
+    redirect_to root_path
   end
 
   private
 
   def prototype_params
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    @prototype = Prototype.find(params[:id])
+    unless current_user.id == @prototype.user_id
+      redirect_to action: :index
+    end
   end
 end
