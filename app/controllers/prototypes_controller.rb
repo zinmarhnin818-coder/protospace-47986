2e3abe7acba56efa19cb7d17dfe8abdef26e7ee5
zinmarhnin818-coder <zinmarhnin818@gmail.com>
2026-08-08 09:43:13 +0900

    コメント投稿機能の実装

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -5,6 +5,8 @@ class PrototypesController < ApplicationController
 
   def show
     @prototype = Prototype.find(params[:id])
+  @comment = Comment.new
+  @comments = @prototype.comments.includes(:user)
   end
 
   def new
